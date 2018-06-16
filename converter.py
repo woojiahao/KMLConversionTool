2ac4ce3074117ee4646833d21c6fb89e83b5c019
Woo Jia Hao <woojiahao1234@gmail.com>
2018-06-17 07:25:15 +0800

    Added a method to check if the file is .kml file

--- converter.py
+++ converter.py
@@ -1,12 +1,26 @@
 from bs4 import BeautifulSoup
 
-def convert (file_url : str):
+
+def is_convertable(file_url: str):
+	extension_position = file_url.rfind('.')
+	if extension_position == -1:
+		# No file extension
+		return False
+	else:
+		# Check if the file extension is a kml file
+		extension: str = file_url[extension_position + 1:]
+		return extension == 'kml'
+
+
+def convert(file_url: str):
 	file_url = file_url.replace('/', '\\')
 
-	with open(file_url) as before_file:
-		soup = BeautifulSoup(before_file, 'lxml-xml')
-		print(soup.prettify())
-
-
+	if is_convertable(file_url):
+		with open(file_url) as before_file:
+			soup = BeautifulSoup(before_file, 'lxml-xml')
+	else:
+		print('The file is not in the right format, ensure that the file is a .kml file')
 
 convert('data/dengue-cases-central-kml.kml')
+convert('data/dengue-cases-central-kml')
+convert('test.xml')
