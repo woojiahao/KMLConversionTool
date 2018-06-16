53d48074834992bf7c8b6119887533c74c58e89d
Woo Jia Hao <woojiahao1234@gmail.com>
2018-06-17 07:17:33 +0800

    Changed the converter to use BeautifulSoup to read the kml file

--- converter.py
+++ converter.py
@@ -1,6 +1,12 @@
+from bs4 import BeautifulSoup
+
 def convert (file_url : str):
 	file_url = file_url.replace('/', '\\')
-	print(file_url)
+
+	with open(file_url) as before_file:
+		soup = BeautifulSoup(before_file, 'lxml-xml')
+		print(soup.prettify())
+
 
 
 convert('data/dengue-cases-central-kml.kml')
