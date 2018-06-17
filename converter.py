9f39918517f4c5d00e75d751fed95a41d285fe5c
Woo Jia Hao <woojiahao1234@gmail.com>
2018-06-17 08:35:28 +0800

    Completed .csv file creator

--- converter.py
+++ converter.py
@@ -1,5 +1,7 @@
-from bs4 import BeautifulSoup
+import os
+
 import numpy as np
+from bs4 import BeautifulSoup
 
 
 def is_convertable(file_url: str):
@@ -22,7 +24,7 @@ def extract_columns(soup: BeautifulSoup):
 	return [col.get('name') for col in soup.find_all('SimpleField')]
 
 
-def extract_data(soup : BeautifulSoup):
+def extract_data(soup: BeautifulSoup):
 	result = ''
 	placemarks = soup.find_all('Placemark')
 	for placemark in placemarks:
@@ -44,15 +46,29 @@ def convert_file(file_url: str):
 	return contents
 
 
+def create_file(kml_url: str, contents: str):
+	prefix = kml_url.rfind('\\')
+	if prefix != -1:
+		kml_url = kml_url[prefix + 1:]
+
+	csv_url = kml_url.replace('.kml', '.csv')
+	destination_folder = os.path.expanduser('~\\Documents\\KMLConverter\\converted\\csv\\')
+	csv_url = '{}{}'.format(destination_folder, csv_url)
+
+	if not os.path.exists(destination_folder):
+		os.makedirs(destination_folder)
+
+	with open(csv_url, 'w+') as new_file:
+		new_file.write(contents)
+
+
 def convert(file_url: str):
 	file_url = file_url.replace('/', '\\')
 
 	if is_convertable(file_url):
-		converted = convert_file(file_url)
+		create_file(file_url, convert_file(file_url))
 	else:
 		print('{} is not in the right format, please ensure that it is a .kml file'.format(file_url))
 
 
 convert('data/dengue-cases-central-kml.kml')
-# convert('data/dengue-cases-central-kml')
-# convert('test.xml')
