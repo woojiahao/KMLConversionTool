82f4109f9d0d7aa1435889dfd072e83626a79a55
Woo Jia Hao <woojiahao1234@gmail.com>
2018-06-19 22:06:34 +0800

    Added CSVConverter2 which now uses OrderedDict to convert the .kml to .csv file. Yay

--- csv_converter_2.py
+++ csv_converter_2.py
@@ -1,30 +1,41 @@
 import os
+from collections import OrderedDict
 
-import numpy as np
 from bs4 import BeautifulSoup
 
 from converter import Converter
 from logger import Logger
 
 
-class CSVConverter(Converter):
+class CSVConverter2(Converter):
 	def __init__(self):
 		self.__logger = Logger()
+		self.__data = OrderedDict()
 
 	def __extract_columns(self, soup: BeautifulSoup):
-		return [col.get('name') for col in soup.find_all('SimpleField')]
+		columns = soup.find_all('SimpleField')
+		for col in columns:
+			self.__data[col.get('name')] = []
 
 	def __extract_data(self, soup: BeautifulSoup):
-		result = ''
 		placemarks = soup.find_all('Placemark')
 		for placemark in placemarks:
 			extended_data = placemark.ExtendedData.SchemaData.find_all('SimpleData')
-			rows = np.ndarray.flatten(np.array([data.contents for data in extended_data])).tolist()
-			result += ','.join(rows)
-			result += '\n'
-		return result
+			for simple_data in extended_data:
+				name: str = simple_data.get('name')
+				val: str = '|'.join([data for data in simple_data.contents])
+				if name in self.__data.keys():
+					self.__data[name].append(val)
 
-	def __create_file(self, kml_url: str, contents: str):
+	def __consolidate_data(self):
+		headers: str = ','.join(self.__data.keys())
+		body: list = []
+		for i in range(len(self.__data.values())):
+			body.append(','.join([self.__data[key][i] for key in self.__data.keys()]))
+
+		return '{}\n{}'.format(headers, '\n'.join(body))
+
+	def __create_file(self, kml_url: str):
 		prefix = kml_url.rfind('\\')
 		if prefix != -1:
 			kml_url = kml_url[prefix + 1:]
@@ -40,24 +51,22 @@ class CSVConverter(Converter):
 			os.makedirs(destination_folder)
 
 		with open(csv_url, 'w+') as new_file:
-			new_file.write(contents)
+			new_file.write(self.__consolidate_data())
 
 		self.__logger.suc('Successfully converted the file, you can find it at {}'.format(csv_url))
-
-	def __convert_file(self, file_url: str):
-		soup = self._get_soup(file_url)
-		return '{}\n{}'.format(','.join(self.__extract_columns(soup)), self.__extract_data(soup))
 
 	def convert(self, file_url: str):
 		file_url = file_url.replace('/', '\\')
 
 		if self._is_convertable(file_url):
-			contents = self.__convert_file(file_url)
-			if contents.strip() != '':
-				self.__create_file(file_url, contents)
+			soup = self._get_soup(file_url)
+			self.__extract_columns(soup)
+			self.__extract_data(soup)
+
+			if len(self.__data) > 0:
+				self.__create_file(file_url)
 			else:
-				self.__logger.info('The target file couldn\'t be extracted to a CSV file as it doesn\'t contain the information needed.')
+				self.__logger.info(
+					'The target file does not contain any information that is suitable for a .csv file.')
 		else:
 			self.__logger.err('{} is invalid, please ensure that it is a .kml file that exists'.format(file_url))
-
-
--- logger.py
+++ logger.py
@@ -5,3 +5,5 @@ class Logger:
 		messagebox.showerror(message = msg)
 	def suc(self, msg: str):
 		messagebox.showinfo(message = msg)
+	def info(self, msg: str):
+		messagebox.showinfo(message = msg)
